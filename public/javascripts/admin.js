c615ade15bbf50640f3f017891b1bf1a8f00d97c
adcor <adironc@gmail.com>
2016-08-07 17:48:38 -0500

    more route arrangeing

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -454,7 +454,7 @@ var $chartName = $(chtNam);
 				var JSONObj = {"username": $(dropdown).val(), "status": $(memberType).val()};
 				$.ajax({
 					type: "PUT",
-					url: ".",
+					url: "../admin",
 					dataType: "json",
 					data: JSONObj,
 					success: function(fin){
