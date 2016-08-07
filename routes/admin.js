64825973228986a28c27e8425e2ab1546de77962
adcor <adironc@gmail.com>
2016-08-07 17:38:12 -0500

    more route arrangeing

--- routes/admin.js
+++ routes/admin.js
@@ -13,14 +13,7 @@ router.get('/',  function(req, res){
 	}
 	
 })
-router.put('/', function(req, res){
-	var query = {"username": req.body.username};
-	var statusUpdate = {$set:{status: req.body.status}}
-	Account.findOneAndUpdate(query, statusUpdate, {upsert: true}, function(err, person){
-		console.log(person);
-	})
-	console.log(req.body);
-})
+
 
 router.get('/logout', function(req, res) {
 	res.redirect('/')
@@ -31,6 +24,15 @@ router.get('/logout', function(req, res) {
 	req.session.destroy();
 	res.render('index1', { });
 });
+
+router.put('/:user', function(req, res){
+	var query = {"username": req.body.username};
+	var statusUpdate = {$set:{status: req.body.status}}
+	Account.findOneAndUpdate(query, statusUpdate, {upsert: true}, function(err, person){
+		console.log(person);
+	})
+	console.log(req.body);
+})
 
 router.get('/:user', Autho.canDo("view", "administrate"), function(req, res){
 	res.render('admin', {user: req.user});
